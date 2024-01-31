c24458c24ef0fb1e9572ce331e40782ecd29ec6e
agusb19 <agustinbrage19@gmail.com>
2024-01-31 18:46:01 -0300

    feat: add login and booking basic modals

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Text, View } from 'react-native'
+import { Link } from 'expo-router'
 
 export default function HomePage() {
     return (
         <View>
-            <Text>Page</Text>
+            <Link href='/(modals)/login'>Login</Link>
+            <Link href='/(modals)/booking'>Booking</Link>
         </View>
     )
 }
