98eb2f66b90d7a49fc9e36c4c911d6b388e352f5
agusb19 <agustinbrage19@gmail.com>
2024-01-31 18:45:24 -0300

    feat: add and style all tab routes

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,22 +1,73 @@
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+import Ionicons from '@expo/vector-icons/Ionicons';
 import React from 'react';
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Link, Tabs } from 'expo-router';
-import { Pressable } from 'react-native';
-
-// You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
-function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name'];
-  color: string;
-}) {
-  return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
-}
+import { Tabs } from 'expo-router';
+import Color from '@/constants/Color';
 
 export default function TabLayout() {
 
   return (
-    <Tabs>
-      <Tabs.Screen/>
-      <Tabs.Screen/>
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Color.primary,
+        headerTitleAlign: 'center',
+        tabBarLabelStyle: {
+          fontFamily: 'mon-sb'
+        }
+      }}
+    >
+      
+      <Tabs.Screen
+        name='index'
+        options={{
+          tabBarLabel: 'Explore',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name='search' size={size} color={color}/>
+          )
+        }}
+      />
+
+      <Tabs.Screen
+        name='wishlists'
+        options={{
+          tabBarLabel: 'Wishlists',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name='heart-outline' size={size} color={color}/>
+          )
+        }}
+      />
+
+      <Tabs.Screen
+        name='trips'
+        options={{
+          tabBarLabel: 'Trips',
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome5 name='airbnb' size={size} color={color}/>
+          )
+        }}
+      />
+
+      <Tabs.Screen
+        name='inbox'
+        options={{
+          tabBarLabel: 'Inbox',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name='message-outline' size={size} color={color}/>
+          )
+        }}
+      />
+
+      <Tabs.Screen
+        name='profile'
+        options={{
+          tabBarLabel: 'Profile',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name='person-circle-outline' size={size} color={color}/>
+          )
+        }}
+      />
+
     </Tabs>
   );
 }
