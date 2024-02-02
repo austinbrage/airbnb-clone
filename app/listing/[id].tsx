8fda798963484e2cce2e10900a916c3610707a58
agusb19 <agustinbrage19@gmail.com>
2024-02-02 17:58:52 -0300

    feat: add complete listing item page with parallax effect

--- app/listing/[id].tsx
+++ app/listing/[id].tsx
@@ -1,13 +1,279 @@
-import { useLocalSearchParams } from "expo-router"
-import { Text, View } from "react-native"
+import Colors from '@/constants/Color'
+import { useLayoutEffect } from 'react'
+import Animated, { SlideInDown, useAnimatedRef, useScrollViewOffset, useAnimatedStyle, interpolate } from 'react-native-reanimated'
+import defaultStyles from '@/constants/Styles'
+import Ionicons from '@expo/vector-icons/Ionicons'
+import listingsData from '@/assets/data/airbnb-listings.json'
+import { Text, View, Image, StyleSheet, Dimensions, TouchableOpacity, Share } from "react-native"
+import { useNavigation, useLocalSearchParams } from "expo-router"
+import { type Listing } from '@/interfaces/listing'
+
+const IMG_HEIGHT = 300
+const { width } = Dimensions.get('window')
 
 export default function ListingPage() {
 
     const { id } = useLocalSearchParams<{ id: string }>()
+    const listing = (listingsData as Listing[]).find(item => item.id === id) 
+
+    if(listing === undefined) return  
+
+    const navigation = useNavigation()
+
+    const shareListing = async () => {
+        try {
+            await Share.share({
+                title: listing.name,
+                url: listing.listing_url
+            })
+        } catch(err) {
+            console.log(err)
+        }
+    }
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerBackground: () => (
+                <Animated.View style={[styles.header, headerAnimatedStyle]}/>
+            ),
+            headerRight: () => (
+                <View style={styles.bar}>
+                    
+                    <TouchableOpacity
+                        onPress={shareListing}
+                        style={styles.roundButton}
+                    >
+                        <Ionicons name='share-outline' size={22} color='#000'/>
+                    </TouchableOpacity>
+                   
+                    <TouchableOpacity
+                        onPress={shareListing}
+                        style={styles.roundButton}
+                    >
+                        <Ionicons name='heart-outline' size={22} color='#000'/>
+                    </TouchableOpacity>
+
+                </View>
+            ),
+            headerLeft: () => (
+                <TouchableOpacity
+                    onPress={shareListing}
+                    style={styles.roundButton}
+                >
+                    <Ionicons name='chevron-back' size={24} color='#000'/>
+                </TouchableOpacity>
+            )
+        })
+    }, [])
+
+    const scrollRef = useAnimatedRef<Animated.ScrollView>()
+    const scrollOffset = useScrollViewOffset(scrollRef)
+    
+    const imageAnimatedStyle = useAnimatedStyle(() => {
+        return {
+            transform: [
+                {
+                    translateY: interpolate(
+                        scrollOffset.value,
+                        [-IMG_HEIGHT, 0, IMG_HEIGHT],
+                        [-IMG_HEIGHT / 2, 0, IMG_HEIGHT * 0.75]
+                    )
+                }
+            ],
+            scale: interpolate(
+                scrollOffset.value, 
+                [-IMG_HEIGHT, 0, IMG_HEIGHT],
+                [2, 1, 1]
+            )
+        }
+    })
+
+    const headerAnimatedStyle = useAnimatedStyle(() => {
+        return {
+            opacity: interpolate(
+                scrollOffset.value,
+                [0, IMG_HEIGHT / 1.5],
+                [0, 1]
+            )
+        }
+    })
 
     return (
-        <View>
-            <Text>Listing {id}</Text>
+        <View style={styles.container}>
+            <Animated.ScrollView 
+                ref={scrollRef}
+                scrollEventThrottle={16}
+                contentContainerStyle={{
+                    paddingBottom: 100
+                }}
+            >
+
+                <Animated.Image 
+                    style={[styles.image, imageAnimatedStyle]}
+                    source={{ uri: listing.xl_picture_url }}
+                />
+
+                <View style={styles.infoContainer}>
+
+                    <Text style={styles.name}>
+                        {listing.name}
+                    </Text>
+                   
+                    <Text style={styles.location}>
+                        {listing.room_type} in {listing.smart_location}
+                    </Text>
+                    
+                    <Text style={styles.rooms}>
+                        {listing.guests_included} guests · {listing.bedrooms} bedrooms · {listing.beds} bed ·{' '}
+                        {listing.bathrooms} bathrooms
+                    </Text>
+                    
+                    <View style={{ flexDirection: 'row', gap: 4 }}>
+                        <Ionicons name="star" size={16}/>
+                        <Text style={styles.ratings}>
+                        {listing.review_scores_rating / 20} · {listing.number_of_reviews} reviews
+                        </Text>
+                    </View>
+
+                    <View style={styles.divider}/>
+
+                    <View style={styles.hostView}>
+                        <Image 
+                            source={{ uri: listing.host_picture_url }} 
+                            style={styles.host} 
+                        />
+                        <View>
+                            <Text style={{ fontWeight: '500', fontSize: 16 }}>
+                                Hosted by {listing.host_name}
+                            </Text>
+                            <Text>
+                                Host since {listing.host_since}
+                            </Text>
+                        </View>
+                    </View>
+
+                    <View style={styles.divider} />
+
+                    <Text style={styles.description}>
+                        {listing.description}
+                    </Text>
+                </View>
+
+            </Animated.ScrollView>
+
+            <Animated.View style={defaultStyles.footer} entering={SlideInDown.delay(200)}>
+                <View style={styles.footerContainer}>
+                    <TouchableOpacity style={styles.footerText}>
+                        <Text style={styles.footerPrice}>
+                            {listing.price}
+                        </Text>
+                        <Text>
+                            night
+                        </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={[defaultStyles.btn, { paddingHorizontal: 20 }]}>
+                        <Text style={defaultStyles.btnText}>
+                            Reserve
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+            </Animated.View>
+
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff'
+    },
+    image: {
+        height: IMG_HEIGHT,
+        width
+    },
+    infoContainer: {
+        padding: 24,
+        backgroundColor: '#fff',
+    },
+    name: {
+        fontSize: 26,
+        fontWeight: 'bold',
+        fontFamily: 'mon-sb',
+    },
+    location: {
+        fontSize: 18,
+        marginTop: 10,
+        fontFamily: 'mon-sb',
+    },
+    rooms: {
+        fontSize: 16,
+        color: Colors.grey,
+        marginVertical: 4,
+        fontFamily: 'mon',
+    },
+    ratings: {
+        fontSize: 16,
+        fontFamily: 'mon-sb',
+    },
+    divider: {
+        height: StyleSheet.hairlineWidth,
+        backgroundColor: Colors.grey,
+        marginVertical: 16,
+    },
+    host: {
+        width: 50,
+        height: 50,
+        borderRadius: 50,
+        backgroundColor: Colors.grey,
+    },
+    hostView: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 12,
+    },
+    footerContainer: {
+        flexDirection: 'row', 
+        alignItems: 'center',
+        justifyContent: 'space-between'
+    },
+    footerText: {
+        height: '100%',
+        justifyContent: 'center',
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 4,
+    },
+    footerPrice: {
+        fontSize: 18,
+        fontFamily: 'mon-sb',
+    },
+    roundButton: {
+        width: 40,
+        height: 40,
+        borderRadius: 50,
+        backgroundColor: 'white',
+        alignItems: 'center',
+        justifyContent: 'center',
+        color: Colors.primary,
+        borderWidth: StyleSheet.hairlineWidth,
+        borderColor: Colors.grey
+    },
+    bar: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        gap: 10,
+    },
+    header: {
+        backgroundColor: '#fff',
+        height: 100,
+        borderBottomWidth: StyleSheet.hairlineWidth,
+        borderColor: Colors.grey,
+    },
+    description: {
+        fontSize: 16,
+        marginTop: 10,
+        fontFamily: 'mon',
+    },
+});
